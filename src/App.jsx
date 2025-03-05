e01767b5c682f3efb1e118f654cf1840bff6168c
aitheenti <navneelkuppili@gmail.com>
2025-03-04 23:33:11 -0500

    LA-2-added react router, key features section, scroll to. Create Account form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <>
-      <LandingPage />
-    </>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
